6f52e5d75e337681d4b186acce640e363e1d2fd9
Vincent <vincent.moutel@edu.devinci.fr>
2020-12-27 17:27:34 +0100

    Doublespend done

--- Simulation double spend.py
+++ Simulation double spend.py
@@ -2,6 +2,7 @@ from math import *
 from random import *
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 def simulation_doublespend(q,n,z,A,v):
     k=1
@@ -74,5 +75,52 @@ print("Nous allons commencer par vous demander les valeurs que vou voulez attrib
 # v1=float(input())
 
 # (R,T) = simulation_doublespend(q1,n1,z1,A1,v1)
+
 (R,T) = simulation_doublespend(0.30,100,6,100,1)
+
+#Rendement theorique en fonction de q
+'''
+x = np.linspace(0, 0.5, 100)
+y=np.linspace(0,0.5,100)
+for i in range(len(y)):
+    y[i]=attacker_revenue_ratio(10,10,x[i])
+plt.plot(x,y)
+plt.show()
+'''
+
+#Rendement pratique en fonction de q
+'''
+x = np.linspace(0, 0.5, 1000)
+y=np.linspace(0,0.5,1000)
+for i in range(len(y)):
+    y[i]=simulation_doublespend(x[i],1000,10,3,10)[0]
+plt.plot(x,y)
+plt.show()
+'''
+#Rendement theorique en fonction de z
+'''
+x1=[0]*30
+for i in range(len(x1)):
+    x1[i]=i+1
+x = np.array(x1)
+y=[0]*30
+for i in range(len(y)):
+    y[i]=attacker_revenue_ratio(x[i],10,0.245)
+plt.plot(x,y)
+plt.show()
+'''
+
+#Rendement pratique en fonction de z
+'''
+x1=[0]*30
+for i in range(len(x1)):
+    x1[i]=i+1
+x = np.array(x1)
+y=np.array(x1)
+for i in range(len(y)):
+    y[i]=simulation_doublespend(0.3,1000,x[i],3,10)[0]
+plt.plot(x,y)
+plt.show()
+'''
+
 
